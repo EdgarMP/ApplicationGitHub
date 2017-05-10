b29dda9033b13ea384ba5ced9e442a6f03b395c9
Edgar Marco Polo Galvan de la Rosa <egalvan8@gmail.com>
2017-05-10 16:48:09 -0500

    Initial commit

---
+++ app/src/main/java/com/edgarmarcopolo/applicationgithub/MainActivity.java
@@ -0,0 +1,17 @@
+package com.edgarmarcopolo.applicationgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void nuevoMetodo(){
+        String mensaje = "Este es un nuevo método";
+    }
+}
